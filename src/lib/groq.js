7423b3a6fefd0370830607780aa7bc9232f2088b
DOMCHURCH <01dominique.c@gmail.com>
2026-03-17 17:46:29 -0400

    Change GROQ_BASE URL to local API endpoint

--- src/lib/groq.js
+++ src/lib/groq.js
@@ -1,4 +1,4 @@
-const GROQ_BASE = 'https://api.groq.com/openai/v1/chat/completions'
+const GROQ_BASE = '/api/groq'
 const MODEL = 'llama-3.3-70b-versatile'
 
 async function groqChat(messages, json = false) {
