25f7428227443230f23f8b1c9ae66465724238c2
DOMCHURCH <01dominique.c@gmail.com>
2026-03-18 17:48:50 -0400

    Update cerebras.js

--- api/cerebras.js
+++ api/cerebras.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
@@ -10,7 +10,7 @@ module.exports = async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.VITE_CEREBRAS_API_KEY}`,
+        Authorization: `Bearer ${process.env.CEREBRAS_API_KEY}`,
       },
       body: JSON.stringify(req.body),
     })
