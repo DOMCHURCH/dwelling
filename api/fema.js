99da29cc58e0a37e43f4cdba59adce343cfd7bd7
DOMCHURCH <01dominique.c@gmail.com>
2026-03-18 17:27:56 -0400

    Update fema.js

--- api/fema.js
+++ api/fema.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
   if (req.method === 'OPTIONS') return res.status(204).end()
@@ -7,16 +7,11 @@ export default async function handler(req, res) {
     const { geometry, geometryType, spatialRel, outFields, returnGeometry, f } = req.query
     const params = new URLSearchParams({ geometry, geometryType, spatialRel, outFields, returnGeometry, f })
     const url = `https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/28/query?${params}`
-    const response = await fetch(url, {
-      headers: { 'User-Agent': 'DwellingApp/1.0' }
-    })
-    if (!response.ok) {
-      return res.status(200).json({ features: [] })
-    }
+    const response = await fetch(url, { headers: { 'User-Agent': 'DwellingApp/1.0' } })
+    if (!response.ok) return res.status(200).json({ features: [] })
     const data = await response.json()
     res.status(200).json(data)
   } catch (err) {
-    // Return empty instead of 500 so app doesn't crash
     res.status(200).json({ features: [] })
   }
 }
