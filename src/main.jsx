7eab1443be6ed62c78c6f4b222352e994e8b922e
DOMCHURCH <01dominique.c@gmail.com>
2026-03-19 20:12:40 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './lib/errorHandler' // Initialiser le gestionnaire d'erreurs global
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
