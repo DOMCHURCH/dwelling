fcc80f417a5d56d7492f838d49c76818b9143416
DOMCHURCH <01dominique.c@gmail.com>
2026-03-20 15:30:54 -0400

    Create images.js

---
+++ src/images.js
@@ -0,0 +1,17 @@
+import heroNeighborhood    from './hero-neighborhood.webp'
+import architectureTexture from './architecture-texture.webp'
+import neighborhoodNight   from './neighborhood-night.webp'
+import houseNight          from './house-night.webp'
+import mapPins             from './map-pins.webp'
+import priceChart          from './price-chart.webp'
+import citySilhouette      from './city-silhouette.webp'
+
+export const IMG = {
+  hero:         heroNeighborhood,
+  texture:      architectureTexture,
+  neighborhood: neighborhoodNight,
+  house:        houseNight,
+  mapPins:      mapPins,
+  priceChart:   priceChart,
+  city:         citySilhouette,
+}
