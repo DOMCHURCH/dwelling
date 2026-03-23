df57bf90935c54e7c4068c6ee970ff2d81e1b29e
DOMCHURCH <01dominique.c@gmail.com>
2026-03-23 11:57:29 -0400

    Update register.js

--- api/register.js
+++ api/register.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
       email,
       terms_accepted_at: new Date().toISOString(),
       analyses_used: 0,
+      analyses_left: 10,
       is_pro: false,
     }, { onConflict: 'id' })
 
