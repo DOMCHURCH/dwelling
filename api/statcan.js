b3e4be8db42894d6fe50708bbe43d4721964128e
DOMCHURCH <01dominique.c@gmail.com>
2026-03-30 20:01:55 -0400

    Update statcan.js

--- api/statcan.js
+++ api/statcan.js
@@ -4,7 +4,7 @@
 // Also fetches municipal assessment data from Edmonton/Calgary/Vancouver open data
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'https://dwelling-three.vercel.app')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   if (req.method === 'OPTIONS') return res.status(204).end()
