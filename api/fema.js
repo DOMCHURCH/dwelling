ba95c5dfa11752921526fa8d0a29e618531a461c
DOMCHURCH <01dominique.c@gmail.com>
2026-03-30 20:00:34 -0400

    Update fema.js

--- api/fema.js
+++ api/fema.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'https://dwelling-three.vercel.app')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
   if (req.method === 'OPTIONS') return res.status(204).end()
 
