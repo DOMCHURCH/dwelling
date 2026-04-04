548616fd354b4500aab34a3659ec5b8efb285367
DOMCHURCH <01dominique.c@gmail.com>
2026-04-04 10:57:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, memo, lazy, Suspense } from 'react'
-import { useScrollReveal, useCountUp } from './hooks/useScrollReveal'
+import { useScrollReveal } from './hooks/useScrollReveal'
 import { motion, AnimatePresence } from 'framer-motion'
 import AddressSearch from './components/AddressSearch'
 import LoadingState from './components/LoadingState'
@@ -2089,4 +2089,3 @@ export default function App() {
     </div>
   )
 }
-    
